d7118397be567fbe02f287612de97ab3850101f9
Eric Churchill <eric.churchill2@gmail.com>
2017-07-08 19:00:14 -0700

    initial commit

---
+++ lib/ghissues.ex
@@ -0,0 +1,18 @@
+defmodule Ghissues do
+  @moduledoc """
+  Documentation for Ghissues.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ghissues.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ghissues_test.exs
@@ -0,0 +1,8 @@
+defmodule GhissuesTest do
+  use ExUnit.Case
+  doctest Ghissues
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
